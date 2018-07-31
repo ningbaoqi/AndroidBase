0d7cba4210f0e1d95a56435456d4f890cde96b09
ningbaoqi <baoqi.ning@new-bund.net>
2018-07-31 15:24:34 +0800

    Initial commit

---
+++ app/src/main/java/com/shop/ningbaoqi/androidbase/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shop.ningbaoqi.androidbase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
